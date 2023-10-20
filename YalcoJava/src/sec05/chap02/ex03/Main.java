10c1647cdef8699e41342a6b0e7ccb706480a529
EraseDa <fkakrla123@gmail.com>
2023-10-20 11:57:22 +0900

    기초 활용예제들 : 클래스의 인스턴스도 참조 자료형

--- YalcoJava/src/sec05/chap02/ex03/Main.java
+++ YalcoJava/src/sec05/chap02/ex03/Main.java
@@ -11,5 +11,19 @@ public class Main {
 
         ChickenMenu order1 = store1.orderMenu("양념치킨");
         ChickenMenu order2 = store1.orderMenu("오븐구이");
+
+
+        int int1 = 1;
+        int int2 = int1;
+        int1 = 2;  //int1 != int2 ,원시타입이기에
+
+        String str1 = "헬로";
+        String str2 = str1;
+        str1 = "월드"; //str1 != str2, String은 클래스지만 원시타입처럼 취급
+
+        ChickenMenu menu1 = new ChickenMenu("후라이드", 1000);
+        ChickenMenu menu2 = menu1;
+        menu1.price = 1200; //menu1 == menu2
+        System.out.println(menu1==menu2); //true
     }
 }
