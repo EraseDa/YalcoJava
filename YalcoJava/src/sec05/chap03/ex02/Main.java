9bbac9b338d4a764f5a337a9b91d9c3866bc7c0a
EraseDa <fkakrla123@gmail.com>
2023-10-20 12:45:45 +0900

    클래스 필드와 메소드 : 매장 번호 자동 생성

---
+++ YalcoJava/src/sec05/chap03/ex02/Main.java
@@ -0,0 +1,9 @@
+package sec05.chap03.ex02;
+
+public class Main {
+    public static void main(String[] args) {
+        YalcoChicken store1 = new YalcoChicken("판교"); //no=1
+        YalcoChicken store2 = new YalcoChicken("강남"); //no=2
+        YalcoChicken store3 = new YalcoChicken("제주"); //no=3
+    }
+}
